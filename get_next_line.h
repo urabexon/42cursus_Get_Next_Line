d54e3bc202eece754b577e45e9b873a931614dbc
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-07-07 19:20:43 +0900

    add_headerfile

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,30 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 17:37:57 by hurabe            #+#    #+#             */
-/*   Updated: 2024/07/04 17:39:45 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/07/07 19:19:07 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+//include
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+//buffer
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+//prottype
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
