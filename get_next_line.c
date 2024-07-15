2b2ff87a3bdbc56f3721dd9d2b75c65c6e9dae40
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-07-15 21:19:13 +0900

    add

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,114 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 17:37:53 by hurabe            #+#    #+#             */
-/*   Updated: 2024/07/09 19:07:55 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/07/15 21:16:18 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static	char	*null_free(char	**n)
+{
+	free(*n);
+	*n = NULL;
+	return (NULL);
+}
+
+static char	*gnl_result(int num, char **buffer_s)
+{
+	char	*result;
+	char	*save;
+
+	save = NULL;
+	if (num <= 0)
+	{
+		if (**buffer_s == '\0')
+		{
+			free(*buffer_s);
+			*buffer_s = NULL;
+			return (NULL);
+		}
+		result = *buffer_s;
+		*buffer_s = NULL;
+		return (result);
+	}
+	save = ft_substr(*buffer_s, num, BUFFER_SIZE);
+	result = *buffer_s;
+	result[num] = '\0';
+	*buffer_s = save;
+	return (result);
+}
+
+static char	*gnl_read(int fd, char **buffer_save, char *buffer)
+{
+	ssize_t	count_byte;
+	char	*tmp;
+
+	count_byte = 0;
+	tmp = NULL;
+	while (ft_strchr(*buffer_save, '\n') == NULL)
+	{
+		count_byte = read(fd, buffer, BUFFER_SIZE);
+		if (count_byte < 0)
+		{
+			null_free(buffer_save);
+			return (NULL);
+		}
+		if (count_byte == 0)
+			return (gnl_result(count_byte, buffer_save));
+		buffer[count_byte] = '\0';
+		tmp = ft_strjoin(*buffer_save, buffer);
+		if (tmp == NULL)
+			return (null_free(buffer_save), NULL);
+		null_free(buffer_save);
+		*buffer_save = tmp;
+	}
+	return (gnl_result((ft_strchr(*buffer_save, '\n') \
+	- *buffer_save) + 1, buffer_save));
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*save;
 	char		*str;
+	char		*result;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (str == NULL)
+		return (NULL);
+	if (!save)
+	{
+		save = ft_strdup("");
+		if (save == NULL)
+		{
+			free(str);
+			free(save);
+			return (NULL);
+		}
+	}
+	result = gnl_read(fd, &save, str);
+	null_free(&str);
+	return (result);
 }
-
+/*
 int	main(void)
 {
 	int		fd;
-	char	buffer[BUFFER_SIZE];
-	ssize_t	bytesread;
+	int		i;
+	char	*str;
 
 	fd = open("text.txt", O_RDONLY);
-	if (fd == -1)
+	i = 0;
+	while (i < 6)
 	{
-		perror("Failed to open file");
-		return (1);
+		str = get_next_line(fd);
+		printf("%s", str);
+		free(str);
+		i++;
 	}
-
-	while ((bytesread = read(fd, buffer, BUFFER_SIZE - 1)) > 0)
-	{
-		buffer[bytesread] = '\0';
-		printf("%s", buffer);
-	}
-	if (bytesread == -1)
-	{
-		perror("Failed to open file");
-		return (1);
-	}
-
 	close(fd);
 	return (0);
 }
+*/
